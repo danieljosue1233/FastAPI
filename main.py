0d7a6b691d2ffd73338c7a000ba870a5fa59f781
Daniel Martínez <danieljosuemartinezjarquin3@gmail.com>
2026-06-24 15:18:40 -0600

    feat:OT#3 Add Body validation with Pydantic in FastAPI

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import zoneinfo
 from datetime import datetime
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Customer(BaseModel):
+    name: str
+    description: str | None
+    email: str
+    age: int
+
 
 app = FastAPI()
 
@@ -25,3 +34,8 @@ async def time(iso_code: str):
     timezone_str = country_timezones.get(iso)
     tz = zoneinfo.ZoneInfo(timezone_str) if timezone_str else zoneinfo.ZoneInfo("UTC")
     return {"time": datetime.now(tz).isoformat(), "iso_code": iso_code}
+
+
+app.post("/customers")
+async def create_customer(customer_data: Customer):
+    return customer_data
